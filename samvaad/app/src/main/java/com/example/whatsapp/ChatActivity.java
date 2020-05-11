9cc6d68f5d60c6771e741df3eef25a20743ae766
danish-angural <62121923+danish-angural@users.noreply.github.com>
2020-05-11 14:55:46 +1100

    Update ChatActivity.java

--- samvaad/app/src/main/java/com/example/whatsapp/ChatActivity.java
+++ samvaad/app/src/main/java/com/example/whatsapp/ChatActivity.java
@@ -35,7 +35,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ServerValue;
+import  com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
